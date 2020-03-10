517371830c58cd197abeb2e922fb87224618c3d1
kyle <non639@naver.com>
2020-03-10 15:12:02 +0900

    Eclipse 설치 후, 기본 출려과 자주쓰는 단축키 정리입니다.

---
+++ lession01_Basic/src/lession01_Basic/Main.java
@@ -0,0 +1,25 @@
+package lession01_Basic;
+
+/**	자주쓰는 단축키
+ * @author Kyle
+ * 
+ * 	ctrl + space 				: 소스 자동완성
+ * 	ctrl + shift + 숫자패드+/- 	: 글자 크기
+ * 	ctrl + f11 					: 실행
+ * 	ctrl + shift + f 			: 소스 정렬
+ * 	ctrl + alt + 화살표위/아래 		: 해당 라인을 복사해서  붙여넣기
+ * 	ctrl + / 					: 주석생성 
+ * 	ctrl + d : 줄 삭제				: 해당 라인 삭제
+ *  alt + 화살표 위/아래			: 해당 라인 옮기기
+ *  주석(comment) : 기계어로 번역할 때[컴파일] 제외되는 부분
+ *  한줄만 할때,   // 한줄" 을 사용하며 
+ *  여러줄일 경우, /* 여러줄 * / 을 하면 됩니다.
+ * */
+
+public class Main {
+	public static void main(String[] args) {
+
+		System.out.println("Hello Java World");
+
+	}
+}
